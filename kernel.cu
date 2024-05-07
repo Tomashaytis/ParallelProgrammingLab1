8ece1f145800aedd5fa6bb695fdf1bbdca867b55
pavel <tomashaytis@gmail.com>
2024-05-07 10:59:39 +0400

    Выполнили 4 лабораторную работу

---
+++ kernel.cu
@@ -0,0 +1,206 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <fstream>
+#include <string>
+// �� ����, ����� �� ���
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+//
+
+#define BLOCK_SIZE 20
+
+__global__ void matrix_dot(int* A, int* B, int* C, int size)
+{
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = blockIdx.x;
+    int ty = blockIdx.y;
+    int sum = 0;
+    int ia = size * BLOCK_SIZE * by + size * ty;
+    int ib = BLOCK_SIZE * bx + tx;
+    for (int k = 0; k < size; k++)
+        sum += A[ia + k] * B[ib + k * size];
+    int ic = size * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+    C[ic + size * ty + tx] = sum;
+}
+
+class SquareMatrix
+{
+private:
+    int* _data;
+    size_t _size;
+
+public:
+    SquareMatrix(size_t initial_size = 10)
+    {
+        _size = initial_size;
+        _data = new int[_size * _size];
+    }
+    ~SquareMatrix()
+    {
+        clear();
+    }
+    SquareMatrix(const SquareMatrix& src)
+    {
+        *this = src;
+    }
+    SquareMatrix& operator=(const SquareMatrix& src)
+    {
+        clear();
+        _size = src._size;
+        _data = new int[_size * _size];
+        for (size_t i = 0; i < _size * _size; i++)
+        {
+            _data[i] = src._data[i];
+        }
+        return *this;
+    }
+    bool operator==(const SquareMatrix& rhs) const
+    {
+        if (_size != rhs._size)
+            return false;
+        for (size_t i = 0; i < _size; i++)
+        {
+            if (_data[i] != rhs._data[i])
+                return false;
+        }
+        return true;
+    }
+    int& operator()(size_t i, size_t j)
+    {
+        return _data[i * _size + j];
+    }
+    int operator()(size_t i, size_t j) const
+    {
+        return _data[i * _size + j];
+    }
+    void clear()
+    {
+        delete[] _data;
+        _data = NULL;
+        _size = 0;
+    }
+    size_t size() const
+    {
+        return _size;
+    }
+    void random_fill()
+    {
+        for (size_t i = 0; i < _size * _size; i++)
+        {
+            int k = rand() % 2;
+            (k == 0) ? (k = -1) : (k = 1);
+            _data[i] = rand() % 100 * k;
+        }
+    }
+    void fill(int value)
+    {
+        for (size_t i = 0; i < _size * _size; i++)
+        {
+            _data[i] = value;
+        }
+    }
+    double dot(const SquareMatrix& rhs)
+    {
+        std::chrono::steady_clock::time_point start, end;
+        start = std::chrono::steady_clock::now();
+        SquareMatrix result = SquareMatrix(_size);
+        for (size_t i = 0; i < _size; i++)
+        {
+            for (size_t j = 0; j < _size; j++)
+            {
+                for (size_t k = 0; k < _size; k++)
+                {
+                    result(i, j) += (*this)(i, k) * rhs(k, j);
+                }
+            }
+        }
+        end = std::chrono::steady_clock::now();
+        return std::chrono::duration <double>(end - start).count();
+    }
+    double cuda_dot(SquareMatrix& rhs)
+    {
+        SquareMatrix result = SquareMatrix(_size);
+        int* A = _data;
+        int* B = rhs._data;
+        int* C = result._data;
+
+        int* Adev = NULL;
+        int* Bdev = NULL;
+        int* Cdev = NULL;
+
+        cudaMalloc((void**)&Adev, _size * _size * sizeof(int));
+        cudaMalloc((void**)&Bdev, _size * _size * sizeof(int));
+        cudaMalloc((void**)&Cdev, _size * _size * sizeof(int));
+
+        dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+        dim3 blocks(_size / threads.x, _size / threads.y);
+
+        cudaEvent_t begin, end;
+        float time = 0;
+        cudaEventCreate(&begin);
+        cudaEventCreate(&end);
+
+        cudaEventRecord(begin, 0);
+        cudaMemcpy(A, Adev, _size * _size * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(B, Bdev, _size * _size * sizeof(int), cudaMemcpyHostToDevice);
+        matrix_dot <<<blocks, threads>>> (Adev, Bdev, Cdev, _size);
+        cudaMemcpy(C, Cdev, _size * _size * sizeof(int), cudaMemcpyDeviceToHost);
+        cudaEventRecord(end, 0);
+        cudaEventSynchronize(end);
+        cudaEventElapsedTime(&time, begin, end);
+
+        cudaEventDestroy(begin);
+        cudaEventDestroy(end);
+        cudaFree(Adev);
+        cudaFree(Bdev);
+        cudaFree(Cdev);
+        return time;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const SquareMatrix& data)
+    {
+        for (size_t i = 0; i < data.size(); i++)
+        {
+            for (size_t j = 0; j < data.size(); j++)
+            {
+                os << data(i, j) << '\t';
+            }
+            os << '\n';
+        }
+        return os;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    srand(time(0));
+    int size = 1000;
+    int block_sizes[] = {4, 8, 12, 16, 32, 48, 96 };
+    std::string filename;
+    for (int i = 0; i < 7; ++i) {
+        filename = std::to_string(block_sizes[i]);
+        filename += ".txt";
+        std::ofstream file(filename);
+#ifdef BLOCK_SIZE
+#undef BLOCK_SIZE
+#define BLOCK_SIZE block_sizes[i]
+#endif
+        for (int j = 1; j < 11; ++j) {
+            SquareMatrix matrix1(j * 96), matrix2(j * 96), matrix3(j * 96);
+            for (int k = 0; k < 10; ++k) {
+                matrix1.random_fill();
+                matrix2.random_fill();
+                matrix3 = matrix1;
+                file << matrix1.cuda_dot(matrix2) << " ";
+                matrix3.dot(matrix2);
+                if (!(matrix1 == matrix3)) {
+                    std::cout << "Matrices are not equal" << std::endl;
+                    return 1;
+                }
+            }
+        }
+        file.close();
+    }
+    return 0;
+}
--- statistics.py
+++ statistics.py
@@ -1,16 +1,40 @@
+import os.path
 import scipy.stats as sts
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Tuple, List
 
-STATS_FOLDER = "D:\\Projects\\Visual Studio\\source\\repos\\3 курс\\ParallelProgramming-Lab1\\matrices\\statistics\\"
+BLOCK_4 = os.path.join('results', '4.txt')
+BLOCK_8 = os.path.join('results', '8.txt')
+BLOCK_12 = os.path.join('results', '12.txt')
+BLOCK_16 = os.path.join('results', '16.txt')
+BLOCK_32 = os.path.join('results', '32.txt')
+BLOCK_48 = os.path.join('results', '48.txt')
+BLOCK_96 = os.path.join('results', '96.txt')
+SEQUENCE = os.path.join('results', 'sequence_production.txt')
 GAMMA = 0.95
 
-if __name__ == '__main__':
+
+def calculate_statistics(statistic_file: str, gamma: float, description: str) -> Tuple[List[float], List[float]]:
+    """
+    The function calculates statistics for samples of matrix multiplication times
+    :param description: description for input
+    :param statistic_file: file with sample of matrix multiplication times
+    :param gamma: gamma value for confidence interval
+    :return: list of matrix sizes and list of average time of matrix multiplication for this matrix sizes
+    """
     x, y = [], []
-    for k in range(1, 11):
-        with open(STATS_FOLDER + f'{k * 100}.txt', 'r') as fp:
-            sample_t = list(map(float, fp.readlines()))
-        print(f'Matrices {k * 100}x{k * 100}:')
+    with open(statistic_file, 'r') as fp:
+        data = list(map(float, fp.readline().split()))
+        if len(data) == 1:
+            data = list(map(float, fp.readlines()))
+    if description != 'Sequence production':
+        for i in range(len(data)):
+            data[i] /= 1000
+    size = len(data) // 10
+    for k in range(10):
+        k1 = k + 1
+        sample_t = data[k * size:(k + 1) * size]
         sample_t = sorted(sample_t)
         py_me = np.mean(sample_t)
         py_median = np.median(sample_t)
@@ -18,23 +42,112 @@ if __name__ == '__main__':
         py_std = np.std(sample_t)
         py_skewness = sts.skew(sample_t)
         py_kurtosis = sts.kurtosis(sample_t)
-        print("Mean:", py_me)
-        print("Median:", py_median)
-        print("Dispersion:", py_disp)
-        print("STD:", py_std)
-        print("Skewness:", py_skewness)
-        print("Kurtosis:", py_kurtosis)
         volume = len(sample_t)
         s = np.sqrt(volume / (volume - 1)) * np.std(sample_t)
-        t = sts.t.ppf(df=volume - 1, q=(GAMMA + 1) / 2)
+        t = sts.t.ppf(df=volume - 1, q=(gamma + 1) / 2)
         delta = t * s / np.sqrt(volume)
-        print(f'Confidence interval for GAMMA = {GAMMA}: ({py_me - delta}, {py_me + delta})')
-        print()
-        x.append(k * 100)
+        if k == 9:
+            print(description)
+            print(f'Matrices {k1 * 96}x{k1 * 96}:')
+            print("Mean:", py_me)
+            print("Median:", py_median)
+            print("Dispersion:", py_disp)
+            print("STD:", py_std)
+            print("Skewness:", py_skewness)
+            print("Kurtosis:", py_kurtosis)
+            print(f'Confidence interval for GAMMA = {gamma}: ({py_me - delta}, {py_me + delta})')
+            print()
+        x.append(k1 * 96)
         y.append(py_me)
+    return x, y
+
+
+if __name__ == '__main__':
+    matrix_size4, time4 = calculate_statistics(BLOCK_4, GAMMA, 'Block size: 4')
+    matrix_size8, time8 = calculate_statistics(BLOCK_8, GAMMA, 'Block size: 14')
+    matrix_size12, time12 = calculate_statistics(BLOCK_12, GAMMA, 'Block size: 12')
+    matrix_size16, time16 = calculate_statistics(BLOCK_16, GAMMA, 'Block size: 16')
+    matrix_size32, time32 = calculate_statistics(BLOCK_32, GAMMA, 'Block size: 32')
+    matrix_size48, time48 = calculate_statistics(BLOCK_48, GAMMA, 'Block size: 48')
+    matrix_size96, time96 = calculate_statistics(BLOCK_96, GAMMA, 'Block size: 96')
+    matrix_size_s, time_s = calculate_statistics(SEQUENCE, GAMMA, 'Sequence production')
+
+    fig1 = plt.figure(figsize=(10, 6))
     plt.title('Зависимость времени перемножения матриц от их размера')
     plt.xlabel('Размер матрицы')
     plt.ylabel('Время перемножения матриц, с')
-    plt.plot(x, y)
+    plt.plot(matrix_size4, time4)
+    plt.plot(matrix_size8, time8)
+    plt.plot(matrix_size12, time12)
+    plt.plot(matrix_size16, time16)
+    plt.plot(matrix_size32, time32)
+    plt.plot(matrix_size48, time48)
+    plt.plot(matrix_size96, time96)
+    plt.legend(['Блок 4x4', 'Блок 8x8', 'Блок 12x12', 'Блок 16x16', 'Блок 32x32', 'Блок 48x48', 'Блок 96x96'])
     plt.show()
 
+    fig2 = plt.figure(figsize=(10, 6))
+    plt.title('Зависимость времени перемножения матриц от их размера')
+    plt.xlabel('Размер матрицы')
+    plt.ylabel('Время перемножения матриц, с')
+    plt.plot(matrix_size4, time4)
+    plt.plot(matrix_size8, time8)
+    plt.plot(matrix_size12, time12)
+    plt.plot(matrix_size16, time16)
+    plt.plot(matrix_size32, time32)
+    plt.plot(matrix_size48, time48)
+    plt.plot(matrix_size96, time96)
+    plt.semilogy()
+    plt.legend(['Блок 4x4', 'Блок 8x8', 'Блок 12x12', 'Блок 16x16', 'Блок 32x32', 'Блок 48x48', 'Блок 96x96'])
+    plt.show()
+
+    fig3 = plt.figure(figsize=(10, 6))
+    plt.title('Зависимость времени перемножения матриц от их размера')
+    plt.xlabel('Размер матрицы')
+    plt.ylabel('Время перемножения матриц, с')
+    plt.plot(matrix_size_s, time_s)
+    plt.plot(matrix_size4, time4)
+    plt.plot(matrix_size8, time8)
+    plt.plot(matrix_size12, time12)
+    plt.plot(matrix_size16, time16)
+    plt.plot(matrix_size32, time32)
+    plt.plot(matrix_size48, time48)
+    plt.plot(matrix_size96, time96)
+    plt.legend(['Последовательное перемножение', 'Блок 4x4', 'Блок 8x8', 'Блок 12x12', 'Блок 16x16', 'Блок 32x32',
+                'Блок 48x48', 'Блок 96x96'])
+    plt.show()
+
+    fig4 = plt.figure(figsize=(10, 6))
+    plt.title('Зависимость времени перемножения матриц от их размера')
+    plt.xlabel('Размер матрицы')
+    plt.ylabel('Время перемножения матриц, с')
+    plt.plot(matrix_size_s, time_s)
+    plt.plot(matrix_size4, time4)
+    plt.plot(matrix_size8, time8)
+    plt.plot(matrix_size12, time12)
+    plt.plot(matrix_size16, time16)
+    plt.plot(matrix_size32, time32)
+    plt.plot(matrix_size48, time48)
+    plt.plot(matrix_size96, time96)
+    plt.semilogy()
+    plt.legend(['Последовательное перемножение', 'Блок 4x4', 'Блок 8x8', 'Блок 12x12', 'Блок 16x16', 'Блок 32x32',
+                'Блок 48x48', 'Блок 96x96'])
+    plt.show()
+
+    time_per_block = [time4[-1], time8[-1], time12[-1], time16[-1], time32[-1], time48[-1], time96[-1]]
+    block_size = [4, 8, 12, 16, 32, 48, 96]
+    fig5 = plt.figure(figsize=(10, 6))
+    plt.title('Зависимость времени перемножения матриц 960x960 от размера блока')
+    plt.xlabel('Размер блока')
+    plt.ylabel('Время перемножения матриц, с')
+    plt.plot(block_size, time_per_block)
+    plt.show()
+
+    time_per_block = [time4[0], time8[0], time12[0], time16[0], time32[0], time48[0], time96[0]]
+    block_size = [4, 8, 12, 16, 32, 48, 96]
+    fig6 = plt.figure(figsize=(10, 6))
+    plt.title('Зависимость времени перемножения матриц 96x96 от размера блока')
+    plt.xlabel('Размер блока')
+    plt.ylabel('Время перемножения матриц, с')
+    plt.plot(block_size, time_per_block)
+    plt.show()
